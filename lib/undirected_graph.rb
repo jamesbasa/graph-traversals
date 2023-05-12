d8acf85c06996bb0dc1a04f628541fabbee0bd1a
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-11 23:37:53 -0700

    DFS works

--- lib/undirected_graph.rb
+++ lib/undirected_graph.rb
@@ -1,15 +1,50 @@
 class UndirectedGraph
     attr_accessor :matrix
 
+    # down, up, right, left
+    DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
     def initialize(matrix)
         @matrix = matrix
     end
 
     def dfs
-        # TODO
+        return [] if @matrix.empty?
+
+        visited = Set.new
+
+        @num_rows, @num_cols = @matrix.length, @matrix[0].length
+
+        (0...@num_rows).each do |row|
+            (0...@num_cols).each do |col|
+                traverse(row, col, visited)
+            end
+        end
+
+        return visited.to_a
     end
 
     def bfs
         # TODO
+    end
+
+    private
+
+    def traverse(row, col, visited)
+        return unless @matrix[row][col] == 1
+        return if visited.include?([row, col])
+
+        visited << [row, col]
+
+        DIRECTIONS.each do |dir|
+            new_row = row + dir[0]
+            new_col = col + dir[1]
+
+            traverse(new_row, new_col, visited) if in_bounds?(new_row, new_col)
+        end
+    end
+
+    def in_bounds?(row, col)
+        row.between?(0, @num_rows - 1) && col.between?(0, @num_cols - 1)
     end
 end
--- spec/undirected_graph_spec.rb
+++ spec/undirected_graph_spec.rb
@@ -1,12 +1,32 @@
 require '../lib/undirected_graph.rb'
 
 RSpec.describe UndirectedGraph do
-    describe "#initialize" do
-        let(:graph) { UndirectedGraph.new(matrix) }
-        let(:matrix) { [[1, 2], [3, 4]] }
+    let(:graph) { UndirectedGraph.new(matrix) }
+    let(:matrix) do
+        [
+            [1, 1, 0],
+            [0, 1, 0],
+            [1, 0, 1]
+        ]
+    end
 
+    describe "#initialize" do
         it "sets the instance vars" do
             expect(graph.matrix).to eq(matrix)
+        end
+    end
+
+    describe "#dfs" do
+        it "outputs array containing the traversed vertices" do
+            expect(graph.dfs).to eq([[0, 0], [0, 1], [1, 1], [2, 0], [2, 2]])
+        end
+
+        context "for an empty matrix" do
+            let(:matrix) { [] }
+
+            it "outputs empty array" do
+                expect(graph.dfs).to eq([])
+            end
         end
     end
 end
